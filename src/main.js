b26364b94c5dd0d48b0718007f7185e25c8b615f
Sean <seanhung2000@gmail.com>
2022-08-16 17:29:52 +0800

    fetch from pinata

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 const basePath = process.cwd();
 const fs = require("fs");
-const { startCreating, buildSetup } = require(`${basePath}/src/hashlip_main.js`)
+const axios = require("axios");
+const { startCreating, buildSetup } = require(`${basePath}/src/hashlip_main.js`);
 
 const layersPath = `${basePath}/layers`;
 const imagesPath = `${basePath}/images`;
 const jsonsPath = `${basePath}/jsons`;
 
-const start = () => {
+const jsonUrl = "https://gateway.pinata.cloud/ipfs/QmRy8bEHAti4DoWqKL6AjEU6yaijTt5s34BwqaZb21Lhcp"
+
+const start = async () => {
     makeLayers();
+    await getImageJson();
     setImageSets(1);
     buildSetup();
     startCreating();
@@ -23,12 +27,31 @@ const makeLayers = () => {
     fs.mkdirSync(`${layersPath}/acc`);
 }
 
+const getImageJson = async () => {
+    if (fs.existsSync(jsonsPath)) {
+        fs.rmSync(jsonsPath, { recursive: true });
+    }
+    fs.mkdirSync(jsonsPath);
+    try {
+        const response = await axios.get("https://gateway.pinata.cloud/ipfs/QmRy8bEHAti4DoWqKL6AjEU6yaijTt5s34BwqaZb21Lhcp");
+        const metaData = response.data;
+        fs.writeFileSync(`${jsonsPath}/${metaData.edition}.json`, JSON.stringify(metaData));
+    } catch (error) {
+        console.error(error);
+        process.exit();
+    }
+}
+
 const setImageSets = (tokenId) => {
     const metaDataFile = fs.readFileSync(`${jsonsPath}/${tokenId}.json`);
     const metaData = JSON.parse(metaDataFile);
     metaData.attributes.forEach( attribute => {
         fs.writeFileSync(`${layersPath}/${attribute.trait_type}/${attribute.value}.png`, fs.readFileSync(`${imagesPath}/${attribute.trait_type}/${attribute.value}.png`));
     });
+}
+
+const uploadToPinata = () => {
+    
 }
 
 module.exports = { start };
